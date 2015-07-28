56bda99ce1c0729e022334bee1fa66dfd2bad657
munyite-megzs <margaret.sitati1@gmail.com>
2015-07-28 13:50:49 +0300

    done with comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
-  resources :articles
+  root to: 'articles#index'
+
+  resources :articles do
+
+  	resources :comments
+  end
 end
